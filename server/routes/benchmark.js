46856e804e6645886c0ddb30eb3bfe4e3598f4f2
Manuel Rojas <mr.manuell@gmail.com>
2018-03-04 14:13:56 +0100

    Finished the GET index route. It returns an array with the date, rdiff and value of SP500, weekly, within the given start and end dates.

--- server/routes/benchmark.js
+++ server/routes/benchmark.js
@@ -6,23 +6,35 @@ const _ = require("lodash");
 const moment = require("moment");
 
 router.get("/:startDate/:endDate", ensureLoggedIn(), function(req, res, next) {
-  const startDate = req.params.startDate;
-  const endDate = req.params.endDate;
+  /**Get stat and end date from params. Ensure that start date is on monday
+  and end date on friday.
+  **/
+  const startDate = moment(req.params.startDate).startOf("isoWeek");
+  const endDate = moment(req.params.endDate)
+    .startOf("isoWeek")
+    .add(4, "days");
+  //Retrieve the whole benchmark data
   Benchmark.find((err, benchmark) => {
-    console.log(res);
     if (err) return next(err);
-    let momentStartDate = moment(
-      _.find(benchmark[0].rdiff, { date: startDate }).date
-    );
-    let momentEndDate = moment(
-      _.find(benchmark[0].rdiff, { date: endDate }).date
-    );
+    //startDateQuery holds the object of the start date with date and rdiff value
+    let startDateQuery = _.find(benchmark[0].rdiff, {
+      date: startDate.format("YYYY-MM-DD")
+    });
+    //Create a moment for start date to calculate number of days between start and end
+    let momentStartDate = moment(startDateQuery.date);
+    /**Variables to make the for loop more understandable. Iteration starts at queryStart
+     and will iterate queryLength times**/
+    let queryStart = benchmark[0].rdiff.indexOf(startDateQuery);
+    let queryLength = endDate.diff(momentStartDate, "weeks");
+    let queryData = [];
+    let queryIndex = queryStart;
+    for (var i = queryStart; i <= queryStart + queryLength; i++) {
+      queryData.push(benchmark[0].rdiff[queryIndex]);
+      queryIndex += 5;
+    }
+    console.log("final ", queryData);
 
-    console.log(momentEndDate.diff(momentStartDate, "days"));
-    // let momentEndDate = moment(endDateRdiff.date);
-    // console.log("a", momentEndDate);
-
-    res.json(benchmark);
+    res.json(queryData);
   });
 });
 
