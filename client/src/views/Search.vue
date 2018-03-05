7f1a13c6d172b6728903c1312ee14f65eba2b852
Hanna Mazzoni <hannamaz@gmail.com>
2018-03-05 22:41:56 +0100

    display result in search page

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -1,9 +1,40 @@
 <template>
 <div><pre>{{allResults}}</pre>
+  <div class="container col-lg-11 col-sm-11">
+	  <div class="modal-content ">
+	      	
+    <div class="modal-heading text-center ">
+    <br>
+		<h2>Search portfolios page</h2>
+	  </div>
+    
+    
+  <div class=" d-flex flex-column align-items-center pb-3" >
+   
   
+        <div v-for="result in allResults" :key="result.id" class="col-lg-10 col-sm-10 ">
+        <b-card class="mb-2 mt-3" :title="result.portfolioName"
+          header-tag="header"
+          footer-tag="footer">
+        <h6 slot="header" class="mb-0">Header</h6>
+        <em slot="footer">Rating</em>
+        <p class="card-text">{{result.description}} </p>
+        <b-button href="#" variant="primary">Get the details</b-button>
+        </b-card>
+         </div>
+   
+   
   
-<h2>This is the search portfolios page</h2>
-</div>
+
+
+    </div>
+
+
+  </div>
+  </div>
+  </div>
+  
+
 </template>
 
 
