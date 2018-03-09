5f2838fa4f6b6979e9a206a198140ef4459f98e0
Manuel Rojas <mr.manuell@gmail.com>
2018-03-09 13:30:40 +0100

    Changing about page

--- client/src/components/PortfolioGenericCard.vue
+++ client/src/components/PortfolioGenericCard.vue
@@ -4,38 +4,32 @@
  <b-card border-variant="light" class="col-sm-12 col-lg-12 card" :title="portfolio.portfolioName"
           header-tag="header"
           footer-tag="footer">
-
         <h6 slot="header" class="mb-0">
           <div class="d-flex flex-row justify-content-between">
             <div>{{totalInvestors}} investors</div>
             <div>{{totalFollowers}} followers</div>
           </div>
         </h6>
-
         <em slot="footer">
           <star-rating class="star-rating" :rating="averageRate" :show-rating="false" @rating-selected="setRating" :star-size="25"></star-rating>
         </em>
-
         <div class="flex-row ">
           <div>
             <p class="card-text">{{portfolio.description}} </p>
-
           </div>
           <span> Risk: {{portfolio.risk}}</span>
-
           <span> Performance: {{portfolio.performancey1}}</span>
           <br/>
           <br/>
           <b-button variant="dark" @click="storePortfolioId">See the details</b-button>
         </div>
-  <br/>
+</b-card>
+</div>
 </div>
 </template>
-
 <script>
 import StarRating from "vue-star-rating";
 import { addRating } from "../api";
-
 export default {
   components: {
     StarRating
@@ -62,12 +56,8 @@ export default {
       this.rate = rating;
       this.portfolioId = this.portfolio._id;
       this.portfolio.ratings.push(this.rate);
-
       this.$emit("rate", { rate: this.rate, portfolioId: this.portfolioId });
     }
   }
 };
 </script>
-
-
-
--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import About from "./views/About.vue";
 import SignUp from "./views/SignUp.vue";
 import LogIn from "./views/LogIn.vue";
 import Profile from "./views/Profile.vue";
@@ -11,6 +10,7 @@ import Dashboard from "./views/Dashboard.vue";
 import Search from "./views/Search.vue";
 import Risk from "./views/RiskQuestionaire.vue";
 import PortfolioDetails from "./views/PortfolioDetails.vue";
+import SearchNew from "./views/SearchNew.vue";
 
 Vue.use(Router);
 
